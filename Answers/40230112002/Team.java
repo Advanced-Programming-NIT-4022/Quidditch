d84c83ef37064d44c348d0bc3cdb205244aca38a
xofError <sadraa772@gmail.com>
2024-04-08 02:15:46 +0330

    Logic and Constructor for Team and Match added

--- Answers/40230112002/Team.java
+++ Answers/40230112002/Team.java
@@ -3,23 +3,46 @@ public class Team {
     private Keeper keeper;
     private Seeker seeker;
 
-    private Chaser chaser1;
-    private Chaser chaser2;
-    private Chaser chaser3;
+    private Chaser[] chasers;
 
     private Beater beater1;
     private Beater beaser2;
     private int goals;
 
+    public Team(Keeper keeper, Seeker seeker, Chaser[] chasers, Beater beater1 , Beater beater2) {
+        this.keeper = keeper;
+        this.seeker = seeker;
+        this.chasers = chasers;
+        this.beater1 = beater1;
+        this.beaser2 = beater2;
+        this.goals = 0;
+    }
+
+
     private void setGoals(){
         goals++;
     }
 
+    private boolean twoChasersSuccess(){
+        int count = 0;
+        for(Chaser chaser : chasers){
+            if(chaser.isSuccessful()){
+                count++;
+            }
+            if(count >= 2){
+                return true;
+            }
+        }
+        return false;
+    }
     public void play(){
-        if(keeper.isSuccessful() && seeker.isSuccessful() && (beater1.isSuccessful() || beaser2.isSuccessful()) && (chaser1.isSuccessful())|| chaser2.isSuccessful() || chaser3.isSuccessful()){
+        if(keeper.isSuccessful() && seeker.isSuccessful() && (beater1.isSuccessful() || beaser2.isSuccessful()) && twoChasersSuccess()){
             setGoals();
 
         }
     }
 
+    public int getGoals() {
+        return goals;
+    }
 }
