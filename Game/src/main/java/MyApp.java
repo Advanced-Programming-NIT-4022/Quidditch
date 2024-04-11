6c5a8cce4c255fce15390fdc1b02e530ede2c12d
Anndraj <amirrezasolhjo2004@gmail.com>
2024-04-11 17:31:26 +0330

    creat the myapp class then start the match in this main body

---
+++ Game/src/main/java/MyApp.java
@@ -0,0 +1,26 @@
+public class MyApp {
+    public static void main(String[] args) {
+        Keeper keep1 = new Keeper("Keeper1" , 1);
+        Seeker seek1 = new Seeker("Seeker1" , 2);
+        Chaser[] chasers1 = {
+                new Chaser("Chaser1" , 3) , new Chaser("Chaser2" , 4) , new Chaser("Chaser3" , 5)
+        };
+        Beater[] beaters1 = {
+                new Beater("Beater1", 6), new Beater("Beater2", 7)
+        };
+        Keeper keeper2 = new Keeper("Keeper2", 8);
+        Seeker seeker2 = new Seeker("Seeker2", 9);
+        Chaser[] chasers2 = {
+                new Chaser("Chaser4", 10), new Chaser("Chaser5", 11), new Chaser("Chaser6", 12)
+        };
+        Beater[] beaters2 = {
+                new Beater("Beater3", 13), new Beater("Beater4", 14)
+        };
+
+        Team team1 = new Team(keep1, seek1, chasers1, beaters1);
+        Team team2 = new Team(keeper2, seeker2, chasers2, beaters2);
+
+        Match match = new Match(team1, team2);
+        match.start();
+    }
+}
