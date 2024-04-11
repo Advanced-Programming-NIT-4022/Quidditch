463f077ace6af12b2762053f43fb611fe4e1483b
Anndraj <amirrezasolhjo2004@gmail.com>
2024-04-11 15:49:58 +0330

    Creat chaser class then extend the player class and have constructor for it then override the interface for 30 percent

---
+++ Game/src/main/java/Chaser.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+class Chaser extends Player{
+    public Chaser(String name , int number){
+        super(name , number);
+    }
+    @Override
+    public boolean isSuccessful(){
+        return new Random().nextInt(100) < 5;
+    }
+}
