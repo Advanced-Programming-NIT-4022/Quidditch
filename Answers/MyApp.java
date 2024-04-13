a988aa57cdd42a33ec78801d39ba995113254758
Armita Kazemi <armitakazemi55320@gmail.com>
2024-04-13 23:48:53 +0330

    class team implemented

--- Answers/MyApp.java
+++ Answers/MyApp.java
@@ -55,3 +55,45 @@ class Beater extends Player implements Success{
         return Math.random() < Stop_Chance;
     }
 }
+class Team{
+    private String teamName;
+    private Keeper keeper;
+    private Seeker seeker;
+    private Chaser[] chaser;
+    private Beater[] beater;
+    private int goalsScored;
+
+    public Seeker getSeeker(){return seeker;}
+    public Team(String teamName,Keeper keeper,Seeker seeker,Chaser[] chasers,Beater[] beaters){
+        this.teamName=teamName;
+        this.keeper=keeper;
+        this.seeker=seeker;
+        this.chaser=chasers;
+        this.beater=beaters;
+        this.goalsScored=0;
+    }
+    private void setGoal(){ goalsScored++;}
+    public void Play(){
+        int successfulChasers=0;
+        int successfulBeaters=0;
+
+        for (Chaser chaser : chasers){
+            if(chaser.isSuccessful()){
+                successfulChasers++;
+            }
+        }
+       for (Beater beater : beaters){
+           if(beater.isSuccessful()){
+               successfulBeaters++;
+           }
+       }
+       if(keeper.isSuccessful() && successfulBeaters>=1 && successfulChasers>=2){
+           setGoal();
+       }
+    }
+    public String getTeamName(){return teamName;}
+    public int getGoalsScored(){return goalsScored;}
+    public void setSeeker(Seeker seeker){
+        this.seeker=seeker;
+    }
+}
