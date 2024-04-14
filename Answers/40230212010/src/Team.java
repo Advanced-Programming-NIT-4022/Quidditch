3cd4a832020fe7565a6de98792957366cac21c63
mhmd <m.alahbakhshi83@gmail.com>
2024-04-14 21:16:20 +0330

    Team

--- Answers/40230212010/src/Team.java
+++ Answers/40230212010/src/Team.java
@@ -1,3 +1,48 @@
 public class Team {
+    Keeper keeper;
+    Seeker seeker;
+    Chaser[] chasers = new Chaser[3];
+    Beater[] beaters = new Beater[2];
+    private int goals;
 
+    Team(Keeper keeper, Seeker seeker, Chaser[] chasers, Beater[] beaters) {
+        this.keeper = keeper;
+        this.seeker = seeker;
+        this.chasers = chasers;
+        this.beaters = beaters;
+    }
+
+    private void setGoal() {
+        goals++;
+    }
+
+    void play() {
+        boolean keeperSuccess = keeper.isSuccessful();
+        int successfulBeaters = 0;
+        int successfulChasers = 0;
+
+        for (Beater beater : beaters) {
+            if (beater.isSuccessful()) {
+                successfulBeaters++;
+            }
+        }
+
+        for (Chaser chaser : chasers) {
+            if (chaser.isSuccessful()) {
+                successfulChasers++;
+            }
+        }
+
+        if (keeperSuccess && successfulBeaters >= 1 && successfulChasers >= 2) {
+            setGoal();
+        }
+    }
+
+    int getGoals() {
+        return goals;
+    }
+
+    int getScore() {
+        return goals * 10;
+    }
 }
