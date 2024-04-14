311d6f2d8f277b318fc99f3e1243a7e42f9e0e0e
mhmd <m.alahbakhshi83@gmail.com>
2024-04-14 21:15:52 +0330

    Match

--- Answers/40230212010/src/Match.java
+++ Answers/40230212010/src/Match.java
@@ -1,3 +1,32 @@
 public class Match {
+    
+    Team team1;
+    Team team2;
+
+    Match(Team team1, Team team2) {
+        this.team1 = team1;
+        this.team2 = team2;
+    }
+
+    void start() {
+        for (int i = 0; i < 100; i++) {
+            team1.play();
+            team2.play();
+        }
+
+        int team1Score = team1.getScore();
+        int team2Score = team2.getScore();
+
+        System.out.println("Team 1 score: " + team1Score);
+        System.out.println("Team 2 score: " + team2Score);
+
+        if (team1Score > team2Score) {
+            System.out.println("Team 1 wins!");
+        } else if (team2Score > team1Score) {
+            System.out.println("Team 2 wins!");
+        } else {
+            System.out.println("It's a draw!");
+        }
+    }
 
 }
