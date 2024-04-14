ca70d1edfccf92f00364390e5c5ab823c6ee9286
vania <vania.v@gmail.com>
2024-04-14 12:58:34 +0330

    Keeper

---
+++ Answers/Keeper.java
@@ -0,0 +1,11 @@
+package Answers;
+
+import java.util.Random;
+public class Keeper extends Player {
+    Keeper(String name, int number) {
+        super(name , number);
+    }
+    public boolean isSuccessful() {
+        return (new Random().nextInt(100) < 70); // 70% chance for keeper
+    }
+}
