68a44db1d8ebc843d0f33a4fdfeb6db0d9adc2a6
vania <vania.v@gmail.com>
2024-04-14 13:01:43 +0330

    Player

---
+++ Answers/Player.java
@@ -0,0 +1,13 @@
+package Answers;
+
+public class Player implements Success {
+    String name;
+    int number;
+    Player(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+    public boolean isSuccessful() {
+        return false;
+    }
+}
