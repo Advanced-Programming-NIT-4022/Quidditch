d37abdb99f6f218294cadfb467e1ddf24ea0a985
mhmd <m.alahbakhshi83@gmail.com>
2024-04-14 21:15:41 +0330

    keeper

--- Answers/40230212010/src/Keeper.java
+++ Answers/40230212010/src/Keeper.java
@@ -1,3 +1,13 @@
-public class Keeper {
+public class Keeper extends Player{
+
+    Keeper(String name, int number) {
+        super(name, number);
+        //TODO Auto-generated constructor stub
+    }
+    
+    @Override
+    public boolean isSuccessful() {
+        return Math.random() < 0.7;
+    }
 
 }
