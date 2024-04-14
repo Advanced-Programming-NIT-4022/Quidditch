c27d24319aaad66e861728bbdeeafbd08f1fdafa
Nima <Nimafeli138384@gmail.com>
2024-04-14 09:45:15 +0330

    complete the MyApp class

---
+++ Answers/40230112105/src/main/java/org/example/MyApp.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class MyApp {
+    public static void main(String[] args) {
+        Keeper keeper1 = new Keeper("Keeper 1", 1);
+        Seeker seeker1 = new Seeker("Seeker 1", 2);
+        Chaser[] chasers1 = {new Chaser("Chaser 1", 3), new Chaser("Chaser 2", 4), new Chaser("Chaser 3", 5)};
+        Beater[] beaters1 = {new Beater("Beater 1", 6), new Beater("Beater 2", 7)};
+        Team team1 = new Team(keeper1, seeker1, chasers1, beaters1);
+
+        Keeper keeper2 = new Keeper("Keeper 2", 1);
+        Seeker seeker2 = new Seeker("Seeker 2", 2);
+        Chaser[] chasers2 = {new Chaser("Chaser 4", 3), new Chaser("Chaser 5", 4), new Chaser("Chaser 6", 5)};
+        Beater[] beaters2 = {new Beater("Beater 3", 6), new Beater("Beater 4", 7)};
+        Team team2 = new Team(keeper2, seeker2, chasers2, beaters2);
+
+        Match match = new Match(team1, team2);
+        match.start();
+    }
+}
