d17f238cc803084d451e6d92439484b274dbea7b
mahdiyar <mahdiyarkhazaii@gmail.com>
2024-04-14 23:27:24 +0330

    done.

--- 40230112057/src/example/Match.java
+++ 40230112057/src/example/Match.java
@@ -5,16 +5,24 @@ public class Match {
     Team team2=new Team();
     public void Start1(){
         if(team1.seeker.isSuccessful()){
+            System.out.println("**************************************");
             System.out.println("team 1 won by finding the golden snitch.");
+            System.out.println("**************************************");
             System.out.println("The Scores:");
+
             System.out.println("team1: "+(team1.goals+150)+"         team2: "+team2.goals);
+            System.out.println("**************************************");
             return;
         }
 
         if(team2.seeker.isSuccessful()){
+            System.out.println("**************************************");
             System.out.println("team 2 won by finding the golden snitch.");
+            System.out.println("**************************************");
             System.out.println("The Scores:");
+
             System.out.println("team1: "+team1.goals+"         team2: "+(team2.goals+150));
+            System.out.println("**************************************");
             return;
 
         }
@@ -24,19 +32,31 @@ public class Match {
             team2.Play();
         }
             if(team1.goals>team2.goals){
+                System.out.println("**************************************");
                 System.out.println("team 1 won by having more goals.");
+                System.out.println("**************************************");
                 System.out.println("The Scores:");
+
                 System.out.println("team1: "+team1.goals+"           team2: "+team2.goals);
+                System.out.println("**************************************");
             }
             else if(team1.goals<team2.goals){
+                System.out.println("**************************************");
                 System.out.println("team 2 won by having more goals.");
+                System.out.println("**************************************");
                 System.out.println("The Scores:");
+
                 System.out.println("team1: "+team1.goals+"           team2: "+team2.goals);
+                System.out.println("**************************************");
             }
             else{
+                System.out.println("**************************************");
                 System.out.println("draw.");
+                System.out.println("**************************************");
                 System.out.println("The Scores:");
+
                 System.out.println("team1: "+team1.goals+"           team2: "+team2.goals);
+                System.out.println("**************************************");
             }
 
 
@@ -46,15 +66,23 @@ public class Match {
             team1.Play();
             team2.Play();
             if(team1.seeker.isSuccessful()){
+                System.out.println("**************************************");
                 System.out.println("team 1 won by finding the golden snitch.");
+                System.out.println("**************************************");
                 System.out.println("The Scores:");
+
                 System.out.println("team1: "+(team1.goals+150)+"         team2: "+team2.goals);
+                System.out.println("**************************************");
                 return;
             }
             if(team2.seeker.isSuccessful()){
+                System.out.println("**************************************");
                 System.out.println("team 2 won by finding the golden snitch.");
+                System.out.println("**************************************");
                 System.out.println("The Scores:");
+
                 System.out.println("team1: "+team1.goals+"         team2: "+(team2.goals+150));
+                System.out.println("**************************************");
                 return;
             }
         }
