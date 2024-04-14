2956aca7a1e2c62bc7a6116c751850381d848da9
mhmd <m.alahbakhshi83@gmail.com>
2024-04-14 21:16:09 +0330

    Seeker

--- Answers/40230212010/src/Seeker.java
+++ Answers/40230212010/src/Seeker.java
@@ -1,3 +1,13 @@
-public class Seeker {
+public class Seeker extends Player{
+
+    Seeker(String name, int number) {
+        super(name, number);
+        //TODO Auto-generated constructor stub
+    }
+    
+    @Override
+    public boolean isSuccessful() {
+        return Math.random() < 0.05;
+    }
 
 }
