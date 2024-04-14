a8c53a327fc70063b3bdf2388c4fc8e52d164694
mhmd <m.alahbakhshi83@gmail.com>
2024-04-14 21:15:04 +0330

    Beater

--- Answers/40230212010/src/Beater.java
+++ Answers/40230212010/src/Beater.java
@@ -1,3 +1,12 @@
-public class Beater {
+public class Beater extends Player{
 
+    Beater(String name, int number) {
+        super(name, number);
+        //TODO Auto-generated constructor stub
+    }
+    
+    @Override
+    public boolean isSuccessful() {
+        return Math.random() < 0.4;
+    }
 }
