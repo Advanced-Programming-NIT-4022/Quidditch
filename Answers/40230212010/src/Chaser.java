5d9f2c8b865fe98d919d8290b63400e698f592d3
mhmd <m.alahbakhshi83@gmail.com>
2024-04-14 21:15:26 +0330

    Chaser

--- Answers/40230212010/src/Chaser.java
+++ Answers/40230212010/src/Chaser.java
@@ -1,3 +1,13 @@
-public class Chaser {
+public class Chaser extends Player{
+
+    Chaser(String name, int number) {
+        super(name, number);
+        //TODO Auto-generated constructor stub
+    }
+    
+    @Override
+    public boolean isSuccessful() {
+        return Math.random() < 0.3;
+    }
 
 }
