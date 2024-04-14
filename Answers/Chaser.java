93536784d446e533bcea54fdd3a49fe79c130122
vania <vania.v@gmail.com>
2024-04-14 12:58:11 +0330

    Chaser

---
+++ Answers/Chaser.java
@@ -0,0 +1,11 @@
+package Answers;
+
+import java.util.Random;
+public class Chaser extends Player {
+    Chaser(String name, int number) {
+        super(name, number);
+    }
+    public boolean isSuccessful() {
+        return (new Random().nextInt(100) < 30); // 30% chance for chaser
+    }
+}
