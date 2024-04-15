0337e215ed1669197898760637c357bbdb2b2e6c
DanialArzani <afm7303@gmail.com>
2024-04-15 21:54:35 +0330

    player class fixed

--- Answers/Quidditch/src/main/java/org/example/Player.java
+++ Answers/Quidditch/src/main/java/org/example/Player.java
@@ -1,9 +1,21 @@
 package org.example;
 
+import java.util.Random;
+
 class Player implements Success{
+
+    int chance ;
+    Random randomNumber = new Random();
+
     @Override
     public boolean isSuccessful() {
 
-        return false;
+        int successChance = randomNumber.nextInt(100)+1 ;
+
+        if(successChance <= chance)
+        {
+            return true;
+        }
+            return false;
     }
 }
