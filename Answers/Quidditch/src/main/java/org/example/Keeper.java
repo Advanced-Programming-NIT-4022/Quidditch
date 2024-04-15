ae6ffa39d74ccee04270e6b624dfeb5209a1a906
DanialArzani <afm7303@gmail.com>
2024-04-15 21:54:24 +0330

    keeper class fixed

--- Answers/Quidditch/src/main/java/org/example/Keeper.java
+++ Answers/Quidditch/src/main/java/org/example/Keeper.java
@@ -1,22 +1,11 @@
 package org.example;
 
-import java.util.Random;
 
-class Keeper extends Player {
+public class Keeper extends Player {
 
 
-    @Override
-    public boolean isSuccessful() {
-        Random randomNumber = new Random();
-        int successChance = randomNumber.nextInt(100);
-        if(successChance < 70)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+    public Keeper(){
+        this.chance = 70;
     }
 
 }
