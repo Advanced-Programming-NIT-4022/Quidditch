a60603c56453595eee5635baad5fdcb0b231713d
DanialArzani <afm7303@gmail.com>
2024-04-15 21:54:46 +0330

    seeker class fixed

--- Answers/Quidditch/src/main/java/org/example/Seeker.java
+++ Answers/Quidditch/src/main/java/org/example/Seeker.java
@@ -1,22 +1,12 @@
 package org.example;
 
-import java.util.Random;
 
-class Seeker extends Player{
+public class Seeker extends Player{
 
-    @Override
-    public boolean isSuccessful() {
-        Random randomNumber = new Random();
-        int successChance = randomNumber.nextInt(100);
-        if(successChance < 5)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+    public Seeker(){
+        this.chance=5;
     }
+
 
 
 }
