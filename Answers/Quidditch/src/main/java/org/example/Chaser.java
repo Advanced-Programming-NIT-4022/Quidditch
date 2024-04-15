feafda8f58074f369fd9054607adab1d3a88b5d1
DanialArzani <afm7303@gmail.com>
2024-04-15 21:53:52 +0330

    chaser class fixed

--- Answers/Quidditch/src/main/java/org/example/Chaser.java
+++ Answers/Quidditch/src/main/java/org/example/Chaser.java
@@ -1,20 +1,11 @@
 package org.example;
 
-import java.util.Random;
 
-class Chaser extends Player {
+public class Chaser extends Player {
 
-    @Override
-    public boolean isSuccessful() {
-        Random randomNumber = new Random();
-        int successChance = randomNumber.nextInt(100);
-        if(successChance < 30)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+    public Chaser(){
+        this.chance = 30;
     }
+
+
 }
